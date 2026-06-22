61a58f88ddcd6645e500e7f79d4f2723aaa6717c
Bogdan Janchencko <robotosoid2000@gmail.com>
2026-06-22 17:02:10 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className={`${montserrat.variable} ${dmSans.variable}`}>
       <body>
 <TanStackProvider>
-  <AppInitializer />
   <Header />
   {children}
   <Footer />
